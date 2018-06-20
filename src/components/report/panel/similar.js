7d00f6277ddb72a4546ce15a417760fb25ef6532
James Ford <psyked84@gmail.com>
2018-06-20 23:55:26 +0100

    Highlight the similar colours to keep

--- src/components/report/panel/similar.js
+++ src/components/report/panel/similar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Segment, Tab, Popup } from 'semantic-ui-react'
+import { Segment, Tab, Popup, Label, Icon } from 'semantic-ui-react'
 
 import styles from './similar.module.css'
 
@@ -12,12 +12,34 @@ export default (props) => <Tab.Pane>
                 return (
                     <Segment key={id} vertical>
                         <div className={styles.palettecontainer}>
-                            {group.map((colour) => {
+                            {group.map(colour => {
+
+                                const sortedGroup = [...group].sort((a, b) => {
+                                    return b.useCount - a.useCount
+                                })
+                                const isKeeper = sortedGroup[0].useCount >= 1 && sortedGroup[0].hex === colour.hex;
+
+                                return {
+                                    ...colour,
+                                    isKeeper
+                                }
+
+                            }).map((colour) => {
                                 return (
                                     <Popup
-                                        trigger={<div key={colour.hex} className={`ui ${styles.palette} ${colour.hsl[2] > 50 ? styles.palette_dark : styles.palette_light}`} style={{ backgroundColor: colour.hex }}>
-                                            {colour.hex.toUpperCase()}
-                                        </div>}
+                                        trigger={
+                                            <div style={{ position: 'relative' }}>
+                                                {/* {colour.useCount <= 1 &&
+                                                    <Label color='red' corner='right'><Icon name='delete' /></Label>
+                                                } */}
+                                                {colour.isKeeper &&
+                                                    <Label color='green' corner='right'><Icon name='check' /></Label>
+                                                }
+                                                <div key={colour.hex} className={`ui ${styles.palette} ${colour.hsl[2] > 50 ? styles.palette_dark : styles.palette_light}`} style={{ backgroundColor: colour.hex }}>
+                                                    {colour.hex.toUpperCase()}
+                                                </div>
+                                            </div>
+                                        }
                                         content={`Used ${colour.useCount} time${colour.useCount > 1 ? 's' : ''}`}
                                         position='top center'
                                         inverted
