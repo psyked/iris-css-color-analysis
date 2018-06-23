f0a12efd0e4902fe6fafdd61e450324a2f847fb1
James Ford <psyked84@gmail.com>
2018-06-23 12:02:42 +0100

    Make the ace-editor fill the tab width

--- src/components/report/panel/source.js
+++ src/components/report/panel/source.js
@@ -2,22 +2,54 @@ import React from 'react'
 import { Tab } from 'semantic-ui-react'
 import AceEditor, { diff as DiffEditor } from 'react-ace'
 import { connect } from "react-redux"
+import Measure from 'react-measure'
 
 import 'brace/mode/css'
 import 'brace/theme/monokai'
 
-export default connect(({ exampleData }) => { return { exampleData } })(({ exampleData }) => <Tab.Pane>
-    <h2 id="source-data">Source data</h2>
-    {/* <DiffEditor
+class SourcePane extends React.Component {
+    state = {
+        dimensions: {
+            width: -1,
+            height: -1
+        }
+    }
+    render() {
+        const { width, height } = this.state.dimensions
+        const { exampleData } = this.props;
+        return <Tab.Pane>
+            <h2 id="source-data">Source data</h2>
+            {/* <DiffEditor
         mode="css"
         theme="monokai"
         width="900px"
         splits={2}
         value={[String(props.exampleData), String(props.newData)]}
     /> */}
-    <AceEditor
-        mode="css"
-        theme="monokai"
-        value={exampleData}
-    />
-</Tab.Pane>)
+            <Measure
+                bounds
+                onResize={(contentRect) => {
+                    this.setState({ dimensions: contentRect.bounds })
+                }}
+            >
+                {({ measureRef }) =>
+                    <div ref={measureRef}>
+                        <AceEditor
+                            mode="css"
+                            theme="monokai"
+                            value={exampleData}
+                            width={`${width}px`}
+                            wrapEnabled={true}
+                        />
+                    </div>
+                }
+            </Measure>
+        </Tab.Pane>
+    }
+}
+
+export default connect(({ exampleData }) => {
+    return {
+        exampleData
+    }
+})(SourcePane)
